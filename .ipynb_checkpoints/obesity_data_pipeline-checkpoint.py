3589a1a32590deecd6636dda484e5d6348d24309
kkuria1 <kevinkuria108@gmail.com>
2024-09-27 17:03:37 -0700

    Added two versions one includes pipeline

--- .ipynb_checkpoints/obesity_data_pipeline-checkpoint.py
+++ .ipynb_checkpoints/obesity_data_pipeline-checkpoint.py
@@ -1,4 +1,4 @@
-#pipeline_notebook.py
+#obesity_data_pipeline.py
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from xgboost import XGBClassifier
 
-#Define a function for data preprocessing
+#functions for data preprocessing
 def create_preprocessor(categorical_cols, numerical_cols):
     """
     Creates a preprocessor for both categorical and numerical features.
@@ -24,7 +24,7 @@ def create_preprocessor(categorical_cols, numerical_cols):
     )
     return preprocessor
 
-#Define functions to create pipelines for each model
+#functions to create pipelines for each model
 
 def create_logistic_pipeline(preprocessor):
     """
@@ -51,10 +51,4 @@ def create_knn_pipeline(preprocessor, n_neighbors=5):
     """
     return Pipeline(steps=[('preprocessor', preprocessor), ('classifier', KNeighborsClassifier(n_neighbors=n_neighbors))])
 
-def create_xgboost_pipeline(preprocessor):
-    """
-    Creates a pipeline for XGBoost with preprocessing.
-    :param preprocessor: Preprocessing steps to be included in the pipeline.
-    :return: Pipeline object for XGBoost classifier.
-    """
-    return Pipeline(steps=[('preprocessor', preprocessor), ('classifier', XGBClassifier())])
+
